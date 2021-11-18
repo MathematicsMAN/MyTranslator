3af5509bea1a82436d0bc645d39218b323ca61fc
MathematicsMAN <damir_a_z@mail.ru>
2021-11-18 13:30:01 +0300

    Base of Flow

--- app/src/main/java/ru/android/mytranslator/ui/activity/MainActivity.kt
+++ app/src/main/java/ru/android/mytranslator/ui/activity/MainActivity.kt
@@ -1,109 +1,60 @@
 package ru.android.mytranslator.ui.activity
 
 import android.os.Bundle
-import android.view.View.GONE
-import android.view.View.VISIBLE
-import androidx.recyclerview.widget.LinearLayoutManager
-import org.koin.androidx.viewmodel.ext.android.viewModel
-import ru.android.mytranslator.AppState
-import ru.android.mytranslator.R
-import ru.android.mytranslator.View
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import ru.android.mytranslator.databinding.AcMainBinding
-import ru.android.mytranslator.ui.MainAdapter
-import ru.android.mytranslator.ui.SearchDialogFragment
-import ru.android.mytranslator.viewmodel.MainViewModel
 
-class MainActivity : BaseActivity<AppState>(), View {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: AcMainBinding
-    private var adapter: MainAdapter? = null
-
-    override val model: MainViewModel by viewModel()
+    private val binding by lazy { AcMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = AcMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.searchFab.setOnClickListener {
-            val searchDialogFragment = SearchDialogFragment.newInstance()
-            searchDialogFragment.setOnSearchClickListener(object :
-                SearchDialogFragment.OnSearchClickListener {
-                override fun onClick(searchWord: String) {
-                    model.getWordDescriptions(searchWord, true)
-                }
-            })
-            searchDialogFragment.show(supportFragmentManager, BOTTOM_SHEET_FRAGMENT_DIALOG_TAG)
-        }
 
-        binding.mainActivityRecyclerview.layoutManager = LinearLayoutManager(applicationContext)
-        adapter = MainAdapter { }
-        binding.mainActivityRecyclerview.adapter = adapter
-    }
-
-    override fun renderData(appState: AppState) {
-        when (appState) {
-            is AppState.Success -> {
-                val dataModel = appState.data
-                if (dataModel.isEmpty()) {
-                    showErrorScreen(getString(R.string.empty_server_response_on_success))
-                } else {
-                    showViewSuccess()
-                    if (adapter == null) {
-                        binding.mainActivityRecyclerview.layoutManager =
-                            LinearLayoutManager(applicationContext)
-                        adapter = MainAdapter { }
-                        binding.mainActivityRecyclerview.adapter = adapter
-                    } else {
-                        adapter!!.submitList(dataModel)
-                    }
-                }
-            }
-            is AppState.Loading -> {
-                showViewLoading()
-                if (appState.progress != null) {
-                    binding.progressBarHorizontal.visibility = VISIBLE
-                    binding.progressBarRound.visibility = GONE
-                    binding.progressBarHorizontal.progress = appState.progress
-                } else {
-                    binding.progressBarHorizontal.visibility = GONE
-                    binding.progressBarRound.visibility = VISIBLE
-                }
-            }
-            is AppState.Error -> {
-                showErrorScreen(appState.t.message)
-            }
+        val flow = getFlow()
+        val flow2 = getFlow2()
+        binding.flowButton.setOnClickListener {
+            startFlow(flow, flow2)
         }
     }
 
-    private fun showErrorScreen(error: String?) {
-        showViewError()
-        binding.errorTextview.text = error ?: getString(R.string.undefined_error)
-        binding.reloadButton.setOnClickListener {
-            model.getWordDescriptions("hi", true)
+    private fun getFlow(): Flow<Int> = flow {
+        Log.d(TAG, "start flow")
+        (1..10).forEach {
+            delay(300)
+            Log.d(TAG, "Flow отдаёт значение $it")
+            emit(it)
         }
     }
 
-    private fun showViewSuccess() {
-        binding.successLinearLayout.visibility = VISIBLE
-        binding.loadingFrameLayout.visibility = GONE
-        binding.errorLinearLayout.visibility = GONE
+    private fun getFlow2() = flow {
+        Log.d(TAG, "start flow2")
+        ('a'..'t').forEach {
+            delay(100)
+            Log.d(TAG, "Flow2 отдаёт значение $it")
+            emit(it)
+        }
     }
 
-    private fun showViewLoading() {
-        binding.successLinearLayout.visibility = GONE
-        binding.loadingFrameLayout.visibility = VISIBLE
-        binding.errorLinearLayout.visibility = GONE
-    }
-
-    private fun showViewError() {
-        binding.successLinearLayout.visibility = GONE
-        binding.loadingFrameLayout.visibility = GONE
-        binding.errorLinearLayout.visibility = VISIBLE
+    private fun startFlow(flow: Flow<Int>, flow2: Flow<Char>) = CoroutineScope(Dispatchers.Main).launch {
+        flow
+            .flowOn(Dispatchers.IO)
+            .map { it * 3 }
+            .filter { it % 2 == 0 }
+            .zip(flow2) { f1, f2 -> "$f1 $f2" }
+            .collect {
+                Log.d(TAG, "Activity получила значение flow: $it")
+            }
     }
 
     companion object {
-        private const val BOTTOM_SHEET_FRAGMENT_DIALOG_TAG =
-            "74a54328-5d62-46bf-ab6b-cbf5fgt0-092395"
+        private val TAG = this::class.java.simpleName
     }
 }
